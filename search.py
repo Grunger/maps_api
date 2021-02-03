66e6332aeff19f8b78aebb850802b97004d11975
Andrew Rogov <Andrewrogov@mail.ru>
2021-02-03 16:12:01 +0500

    Create search.py

---
+++ search.py
@@ -0,0 +1,21 @@
+import sys
+from io import BytesIO
+# Этот класс поможет нам сделать картинку из потока байт
+
+import requests
+from PIL import Image
+from geo import get_map_params
+
+# Пусть наше приложение предполагает запуск:
+# python search.py Москва, ул. Ак. Королева, 12
+# Тогда запрос к геокодеру формируется следующим образом:
+toponym_to_find = " ".join(sys.argv[1:])
+
+map_api_server = "http://static-maps.yandex.ru/1.x/"
+# ... и выполняем запрос
+response = requests.get(map_api_server, params=get_map_params(toponym_to_find))
+
+Image.open(BytesIO(
+    response.content)).show()
+# Создадим картинку
+# и тут же ее покажем встроенным просмотрщиком операционной системы
